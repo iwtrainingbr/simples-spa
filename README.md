7ca54c9c133b2dc136900f56e9b6239887f98fca
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2021-10-02 08:41:48 -0300

    Usando apenas a V2 como principal

--- README.md
+++
@@ -1,2 +0,0 @@
-# simples-spa
-Uma pequena demonstração de como poderia ser uma Single Page Application, SPA
--- js/eventos.js
+++ js/eventos.js
@@ -1,24 +1,28 @@
-const BTN_CADASTRAR = document.getElementById('btn-cadastrar');
-const BTN_LISTAR = document.getElementById('btn-listar');
-const SECTION_CADASTRAR = document.getElementById('section-cadastrar');
-const SECTION_LISTAR = document.getElementById('section-listar');
+const MENU = document.querySelectorAll('[data-component="menu"]');
+const CONTENTS = document.querySelectorAll('[data-component="content"]');
 
-BTN_LISTAR.addEventListener('click', function () {
-  SECTION_CADASTRAR.style.display = 'none';
-  SECTION_LISTAR.style.display = 'block';
+MENU.forEach(function (item) {
+  item.addEventListener('click', function() {
+    let action = item.getAttribute('data-action');
 
-  BTN_LISTAR.classList.remove('btn-outline-dark');
-  BTN_LISTAR.classList.add('btn-dark');
-  BTN_CADASTRAR.classList.remove('btn-dark');
-  BTN_CADASTRAR.classList.add('btn-outline-dark');
+    hideAllContent();
+    document.querySelector(`[data-content="${action}"]`).style.display = 'block';
+
+    resetMenu();
+    item.classList.add('btn-dark');
+    item.classList.remove('btn-outline-dark');
+  });
 });
 
-BTN_CADASTRAR.addEventListener('click', function () {
-  SECTION_LISTAR.style.display = 'none';
-  SECTION_CADASTRAR.style.display = 'block';
-  
-  BTN_CADASTRAR.classList.remove('btn-outline-dark');
-  BTN_CADASTRAR.classList.add('btn-dark');
-  BTN_LISTAR.classList.remove('btn-dark');
-  BTN_LISTAR.classList.add('btn-outline-dark');
-});
+function resetMenu() {
+  MENU.forEach(function(item) {
+    item.classList.remove('btn-dark');
+    item.classList.add('btn-outline-dark');
+  });
+}
+
+function hideAllContent() {
+  CONTENTS.forEach(function(section) {
+    section.style.display = 'none';
+  });
+}
